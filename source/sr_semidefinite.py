fixed NT scaling.
fixed a minor bug in the similarity transformation applied to the self dual embedding.

source/sr_semidefinite.py
@@ -40,6 +40,31 @@ def Prox(x, s, mu, p, q):
     
     return phi0 + phi1
 
+def RotateNewtonSystem(nA, m, n, Q, q):
+    """
+    Rotate newton linear system using I + Q + q
+    I - size of constraints
+    Q - rotation on n x n matrices
+    q - scaling for self dual extra 2 dimensions
+    """
+    
+    vn = int(n * (n + 1) / 2)
+    vn2 = vn + 2
+    
+    A = nA.copy()
+    A[0:m, m:(m+vn)] = np.apply_along_axis(lambda u: svecRotate(u, Q), 1, A[0:m, m:(m+vn)])
+    A[0:m, m+vn] *= q[0]
+    A[0:m, m+vn+1] *= q[1]
+    A[m:(m+vn2), 0:m] = -A[0:m, m:(m+vn2)].T
+    A[m:(m+vn), m+vn] = svecRotate(A[m:(m+vn), m+vn], Q) * q[0]
+    A[m:(m+vn), m+vn+1] = svecRotate(A[m:(m+vn), m+vn+1], Q) * q[1]
+    A[(m+vn):(m+vn2), m:(m+vn)] = -A[m:(m+vn), (m+vn):(m+vn2)].T
+    A[m+vn, m+vn+1] *= q[1] * q[0]
+    A[m+vn+1, m+vn] = -A[m+vn, m+vn+1]
+    
+    return A
+    
+
 def FindSearchDirection(nA, nb, x, s, mu, p, q):
     """
     Find the search direction (newton step)  solving
@@ -64,17 +89,7 @@ def FindSearchDirection(nA, nb, x, s, mu, p, q):
     d = np.sqrt(x[-2:] / s[-2:])
     
     # set the rotated data
-    A = nA.copy()
-    A[0:const_cnt, const_cnt:(const_cnt+trm_vec_var)] = np.apply_along_axis(lambda u: svecRotate(u, D), 1, A[0:const_cnt, const_cnt:(const_cnt+trm_vec_var)])
-    A[0:const_cnt, const_cnt+trm_vec_var] *= d[0]
-    A[0:const_cnt, const_cnt+trm_vec_var+1] *= d[1]
-    A[const_cnt:(const_cnt+len_vec_var), 0:const_cnt] = -A[0:const_cnt, const_cnt:(const_cnt+len_vec_var)].T
-    A[const_cnt:(const_cnt+trm_vec_var), const_cnt+trm_vec_var] = svecRotate(A[const_cnt:(const_cnt+trm_vec_var), const_cnt+trm_vec_var], D)
-    A[const_cnt:(const_cnt+trm_vec_var), const_cnt+trm_vec_var+1] = svecRotate(A[const_cnt:(const_cnt+trm_vec_var), const_cnt+trm_vec_var+1], D)
-    A[(const_cnt+trm_vec_var):(const_cnt+len_vec_var), const_cnt:(const_cnt+trm_vec_var)] = -A[const_cnt:(const_cnt+trm_vec_var), (const_cnt+trm_vec_var):(const_cnt+len_vec_var)].T
-    A[const_cnt+trm_vec_var, const_cnt+trm_vec_var+1] *= d[1]
-    A[const_cnt+trm_vec_var+1, const_cnt+trm_vec_var] = -A[const_cnt+trm_vec_var, const_cnt+trm_vec_var+1]
-    
+    A = RotateNewtonSystem(nA, const_cnt, wdt_mat_var, D, d)
     
     V = Rotate(S, D) / np.sqrt(mu)
     [evals, evecs] = np.linalg.eigh(V)
@@ -95,6 +110,7 @@ def FindSearchDirection(nA, nb, x, s, mu, p, q):
     dx[-2:] *= d
     ds[0:-2] = svecRotate(ds[:-2], Dinv)
     ds[-2:] /= d
+    
     
     alpha = min(1/(3*p+1),1/(6*q+4)) * np.linalg.norm(pvals) ** (-(q+1)/q)
     
@@ -129,11 +145,16 @@ def PDSolve(A, b, c, tau, eps, theta, p, q, search_steps):
     
     n = nt + 2
     
+    itr = 0
+    
     while n * mu >= eps:
         mu = (1 - theta) * mu
         
         while Prox(x, s, mu, p, q) >= tau:
             [dy, dx, ds, alpha] = FindSearchDirection(nA, nb, x, s, mu, p, q)
+            
+            #count the number of newton iterations
+            itr += 1
             
             ls_alpha = LineSearchXS(lambda u,w: Prox(u, w, mu, p, q), x, s, dx, ds, alpha, 2, search_steps)
             
@@ -143,13 +164,15 @@ def PDSolve(A, b, c, tau, eps, theta, p, q, search_steps):
             x += ls_alpha * dx
             s += ls_alpha * ds
     
+    print("Newton steps: " + str(itr))
+    
     if x[-2] > s[-2] and x[-2] > eps:
         """
         P optimal
         """
         x = x[0:-2] / x[-2]
-        print("Optimal Value: " + str(np.dot(c, x)) + " with x = " + str(x))
-        return x
+        print("Optimal Value: " + str(np.dot(c, x)) + " with x = \n" + str(smat(x)))
+        return smat(x)
     elif s[-2] > x[-2] and s[-2] > eps:
         """
         P/D inf